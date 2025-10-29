a42eb06c4a91d502c377db82b31086448cb991a0
kingshhub <formulaking07@gmail.com>
2025-10-29 02:17:54 +0100

    updated PORT

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import app from './app';
 import env from './config/env';
 import { AppDataSource } from './config/orm.config';
 
-const PORT = env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 const initializeApp = async () => {
     try {
