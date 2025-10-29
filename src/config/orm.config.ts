b6bf472190104d2591f4ef39f556c9f3f0757df0
kingshhub <formulaking07@gmail.com>
2025-10-29 14:40:40 +0100

    fixed refresh issue

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,26 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import { DataSource } from 'typeorm';
 import env from './env';
 import { Country } from '../entities/Country';
 import { Status } from '../entities/Status';
-
-const caCertPath = path.join(__dirname, '../certs/ca.pem');
-
-const extra: any = {};
-try {
-    const shouldUseSsl = process.env.DB_SSL === 'true' || fs.existsSync(caCertPath);
-    if (shouldUseSsl) {
-        const caContent = fs.existsSync(caCertPath) ? fs.readFileSync(caCertPath).toString() : undefined;
-        extra.ssl = {
-            ca: caContent,
-            rejectUnauthorized: true,
-        };
-    }
-} catch (err) {
-
-    console.warn('Warning: unable to load DB CA certificate, proceeding without SSL for DB connection.', err);
-}
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
@@ -34,6 +15,4 @@ export const AppDataSource = new DataSource({
     entities: [Country, Status],
     migrations: [],
     subscribers: [],
-
-    extra: Object.keys(extra).length ? extra : undefined,
 });
